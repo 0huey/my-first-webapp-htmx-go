9283b5d86eaecf7034da2148d8117d8b3df25080
0huey <34045167+0huey@users.noreply.github.com>
2025-08-27 19:16:50 -0500

    refactor; move root handler to func; add Render func

--- main.go
+++ main.go
@@ -9,14 +9,21 @@ type Count struct {
 	Count int
 }
 
-func main() {
-	count := Count{Count: 0}
+var count Count
 
-	http.HandleFunc("/", func (w http.ResponseWriter, req *http.Request) {
-		count.Count++
-		tmpl := template.Must(template.ParseFiles("templates/index.html"))
-		tmpl.Execute(w, count)
-	})
+func main() {
+	count = Count{Count: 0}
+
+	http.HandleFunc("/", HandleRoot)
 
 	http.ListenAndServe(":8080", nil)
+}
+
+func HandleRoot(w http.ResponseWriter, req *http.Request) {
+	count.Count++
+	Render(w, "templates/index.html", count)
+}
+
+func Render(w http.ResponseWriter, filename string, context any) {
+	template.Must(template.ParseFiles(filename)).Execute(w, context)
 }
