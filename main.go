bd5e8b46a1e3666ac1fe4a06845bc628f531d8ea
0huey <34045167+0huey@users.noreply.github.com>
2025-08-28 14:56:26 -0500

    contacts form

--- main.go
+++ main.go
@@ -6,31 +6,53 @@ import(
 	"fmt"
 )
 
-type Count struct {
-	Count int
+type Contact struct {
+	Name string
+	Email string
 }
 
-var count Count
+type IndexContext struct {
+	Count int
+	Contacts []Contact
+}
+
+var index_context IndexContext
 
 func main() {
-	count = Count{Count: 0}
+	// some initial data
+	NewContact("john smith", "js@example.com")
+	NewContact("person", "person@example.com")
 
 	http.HandleFunc("/", HandleRoot)
 
-	http.HandleFunc("/count", HandleCount)
+	http.HandleFunc("POST /count", HandleCount)
+
+	http.HandleFunc("POST /contacts", HandleContact)
 
 	http.ListenAndServe(":8080", nil)
 }
 
 func HandleRoot(w http.ResponseWriter, req *http.Request) {
-	Render(w, "index", count)
+	Render(w, "index", index_context)
 }
 
 func HandleCount(w http.ResponseWriter, req *http.Request) {
-	if req.Method == "POST" {
-		count.Count++
-		Render(w, "count", count)
+	index_context.Count++
+	Render(w, "count", index_context)
+}
+
+func HandleContact(w http.ResponseWriter, req *http.Request) {
+	name :=  req.PostFormValue("name")
+	email := req.PostFormValue("email")
+
+	if len(name) == 0 || len(email) == 0 {
+		http.Error(w, "", http.StatusBadRequest)
+		return
 	}
+
+	NewContact(name, email)
+
+	Render(w, "contacts", index_context)
 }
 
 func Render(w http.ResponseWriter, block string, context any) {
@@ -41,4 +63,9 @@ func Render(w http.ResponseWriter, block string, context any) {
 	if err != nil {
 		fmt.Println(err)
 	}
+}
+
+func NewContact(name string, email string) {
+	c := Contact {Name: name, Email: email}
+	index_context.Contacts = append(index_context.Contacts, c)
 }
